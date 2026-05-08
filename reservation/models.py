820e9070e48810f33b39399955d02d224c1d93ed
Karnevall-sk <sergey20sergey2000@gmail.com>
2026-05-08 17:22:04 +0500

    3

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from restaurants.models import RestaurantTable, Restaurant
+from users.models import User
 
-# Create your models here.
+class Reservation(models.Model):
+    
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT)
+
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("confirmed", "Confirmed"),
+        ("cancelled", "Cancelled"),
+        ("completed", "Completed"),
+    ]
+
+    table = models.ForeignKey(RestaurantTable)
+
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name="reservations")
